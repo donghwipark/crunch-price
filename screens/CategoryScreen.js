8d29dec29600e74e9b505bbfb86fa15a40070356
Park Donghwi <parkdonghwi@Parks-MacBook-Pro.local>
2019-02-25 21:38:04 +0900

    category view 20%

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {
-  ScrollView, StyleSheet, Text, Platform,
+  ScrollView, StyleSheet, Text, Platform, View, TouchableHighlight,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import Emoji from 'react-native-emoji';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Colors from '../constants/Colors';
 
 export default class CategoryScreen extends React.Component {
@@ -24,17 +26,97 @@ export default class CategoryScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>ScrollView here</Text>
+      <ScrollView>
+        <ScrollView style={styles.list}>
+          <Text>
+            list
+          </Text>
+        </ScrollView>
+        <TouchableHighlight
+          onPress={console.log('식품')}
+          style={styles.category}
+        >
+          <View>
+            <View style={styles.categoryList}>
+              <Emoji name="hamburger" style={styles.emoji} />
+              <Text style={styles.text}>식품</Text>
+            </View>
+            <View style={styles.categoryList}>
+              <Icon
+                name={
+                  Platform.OS === 'ios'
+                    ? 'ios-arrow-forward'
+                    : 'md-arrow-forward'
+                }
+                size={26}
+                style={styles.arrow}
+                color="grey"
+              />
+            </View>
+          </View>
+        </TouchableHighlight>
+        <View style={styles.categoryList}>
+          <Emoji name="bath" style={styles.emoji} />
+          <Text style={styles.text}>생활/건강</Text>
+        </View>
+        <View style={styles.categoryList}>
+          <Emoji name="baby" style={styles.emoji} />
+          <Text style={styles.text}>출산/육아</Text>
+        </View>
+        <View style={styles.categoryList}>
+          <Emoji name="lipstick" style={styles.emoji} />
+          <Text style={styles.text}>화장품/미용</Text>
+        </View>
+        <View style={styles.categoryList}>
+          <Emoji name="bulb" style={styles.emoji} />
+          <Text style={styles.text}>디지털/가전</Text>
+        </View>
+        <View style={styles.categoryList}>
+          <Emoji name="basketball" style={styles.emoji} />
+          <Text style={styles.text}>스포츠/레저</Text>
+        </View>
+        <View style={styles.categoryList}>
+          <Emoji name="airplane" style={styles.emoji} />
+          <Text style={styles.text}>여행/문화</Text>
+        </View>
+        <View style={styles.categoryList}>
+          <Emoji name="house" style={styles.emoji} />
+          <Text style={styles.text}>가구/인테리어</Text>
+        </View>
+        <View style={styles.categoryList}>
+          <Emoji name="womans_clothes" style={styles.emoji} />
+          <Text style={styles.text}>패션잡화</Text>
+        </View>
       </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+  list: {
+    backgroundColor: 'yellow',
+    height: hp('30%'),
+  },
+  category: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  categoryList: {
+    flexDirection: 'row',
+    margin: 10,
+    backgroundColor: 'yellow',
+    fontSize: 20,
+  },
+  emoji: {
+    fontSize: 15,
+    padding: 5,
+  },
+  text: {
+    fontSize: 15,
+    padding: 5,
+  },
+  arrow: {
+    fontSize: 15,
+    padding: 5,
   },
 });
