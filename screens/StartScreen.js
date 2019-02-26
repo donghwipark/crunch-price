fdc9f985ca9872785951a726638cb50315a202ce
Kim Sejun <sejun3278@gmail.com>
2019-02-26 14:04:19 +0900

    'start-view-complete'

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,30 +1,84 @@
 import React from 'react';
 import {
-  ActivityIndicator,
-  AsyncStorage,
-  Button,
-  StatusBar,
   StyleSheet,
   Text,
+  Image,
   View,
-} from 'react-native'
+  TouchableHighlight,
+} from 'react-native';
 
 export default class StartScreen extends React.Component {
   static navigationOptions = {
-    title: null,  
+    title: null,
   };
 
   render() {
+    const { navigation } = this.props
     return (
-    <View style={{flex:1}}>
-      <Text style={{flex:1}}>Welcome to Crunch Price</Text>
-      <Button
-        onPress={() => {this.props.navigation.navigate('Main')}}
-        title="Move to Index"
-        color="#841584"
-        style={{flex:1}}
-      />
-    </View>
+      <View style={styles.container}>
+        <Image
+          style={styles.norasPhoto}
+          source={require('../assets/images/noras.jpg')}
+        />
+        <Text style={styles.title}> 더-싸지는 쇼핑 </Text>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => { navigation.navigate('Main'); }}
+        >
+          <View style={styles.borders}>
+            <Text style={styles.startTitle}> 시작하기 </Text>
+          </View>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(252, 192, 194)',
+  },
+  title: {
+    flex: 1,
+    marginTop: '15%',
+    padding: '5%',
+    fontSize: 45,
+    lineHeight: 55,
+    color: 'white',
+  },
+  startBotton: {
+    flex: 1,
+    fontSize: 30,
+  },
+  norasPhoto: {
+    position: 'absolute',
+    width: 200,
+    height: 200,
+    marginTop: '60%',
+    marginLeft: '20%',
+  },
+  button: {
+    marginTop: '10%',
+    marginBottom: '15%',
+    marginLeft: '10%',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: '80%',
+    height: '8%',
+  },
+  borders: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderStyle: 'solid',
+  },
+  startTitle: {
+    textAlign: 'center',
+    color: 'rgb(0, 122, 255)',
+    lineHeight: 51,
+    marginTop: '35%',
+    fontSize: 18,
+    marginBottom: 50,
+  },
+});
